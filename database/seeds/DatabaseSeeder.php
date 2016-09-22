65ee4a37f3fb46310a6fa92564f7dcb11c820a6f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-22 18:52:02 +0700

    seeder untuk tabel kelas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PerusahaansTableSeeder::class);
+        $this->call(KelasesTableSeeder::class);
     }
 }
---
+++ database/seeds/KelasesTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KelasesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $isi = [
+          [
+            "nama"  => "Harta (Aktiva)",
+            "jenis" => "NR",
+            "gol"   => "DB"
+          ],
+          [
+            "nama"  => "Kewajiban (Pasiva)",
+            "jenis" => "NR",
+            "gol"   => "CR"
+          ],
+          [
+            "nama"  => "Ekuitas (Modal)",
+            "jenis" => "NR",
+            "gol"   => "CR"
+          ],
+          [
+            "nama"  => "Pendapatan",
+            "jenis" => "LR",
+            "gol"   => "CR"
+          ],
+          [
+            "nama"  => "Biaya atas Pendapatan",
+            "jenis" => "LR",
+            "gol"   => "DB"
+          ],
+          [
+            "nama"  => "Pengeluaran Operasional",
+            "jenis" => "LR",
+            "gol"   => "DB"
+          ],
+          [
+            "nama"  => "Pengeluaran Non Operasional",
+            "jenis" => "LR",
+            "gol"   => "DB"
+          ],
+          [
+            "nama"  => "Pendapatan Lain",
+            "jenis" => "LR",
+            "gol"   => "CR"
+          ],
+          [
+            "nama"  => "Pengeluaran Lain",
+            "jenis" => "LR",
+            "gol"   => "DB"
+          ]
+        ];
+        
+        DB::table('kelases')->insert($isi);
+    }
+}
