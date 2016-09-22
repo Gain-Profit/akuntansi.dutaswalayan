327e6be5eb205a558483225c3bdf613a01e81f67
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-22 18:11:45 +0700

    tabel company diganti tabel perusahaan

--- database/migrations/2016_09_22_000000_create_perusahaans_table.php
+++ database/migrations/2016_09_22_000000_create_perusahaans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCompaniesTable extends Migration
+class CreatePerusahaansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateCompaniesTable extends Migration
      */
     public function up()
     {
-        Schema::create('tb_company', function (Blueprint $table) {
-            $table->string('kd_perusahaan',10);
+        Schema::create('perusahaans', function (Blueprint $table) {
+            $table->string('id',10);
             $table->string('n_perusahaan',30);
             $table->string('alamat',255);
             $table->string('telp',30)->nullable();
             $table->string('email',30)->nullable();
             $table->string('npwp',50)->nullable();
             $table->string('periode_akun',10);            
-            $table->primary('kd_perusahaan');
+            $table->timestamps();
+            $table->primary('id');
         });
     }
 
@@ -32,6 +33,6 @@ class CreateCompaniesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tb_company');
+        Schema::dropIfExists('perusahaans');
     }
 }
