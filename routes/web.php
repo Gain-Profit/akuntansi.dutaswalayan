afd6a93d0d0486d8d52fe15084cbb2b86f4009b2
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-23 18:54:57 +0700

    query untuk neraca dan laba rugi.
    tambah kode perkiraan.

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ Route::get('/neraca/{comp}/{period}', function ($comp,$period) {
     $bulan = substr($period,5);
 
     $hasil = DB::select(
-        'SELECT jh.perusahaan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, kl.nama AS nama_kelas, kl.gol, 
-        IF(kl.gol = "DB",(jh.saldo_awal + jh.debit - jh.kredit),-(jh.saldo_awal + jh.debit - jh.kredit)) AS saldo_ahir 
+        'SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, kl.nama AS nama_kelas, kl.gol, 
+        IF(kl.gol = "DB",(jh.saldo_awal + jh.debit - jh.kredit),-(jh.saldo_awal + jh.debit - jh.kredit)) AS saldo_akhir 
         FROM jurnal_sejarahs jh 
         INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
         INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
@@ -50,8 +50,8 @@ Route::get('/laba/{comp}/{period}', function ($comp,$period) {
     $tahun = substr($period,0,4);
     $bulan = substr($period,5);
 
-    $hasil = DB::select('SELECT jh.perusahaan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, 
-    kl.nama AS nama_kelas, kl.gol, IF(kl.gol = "DB",(jh.debit - jh.kredit),-(jh.debit - jh.kredit)) AS saldo_ahir 
+    $hasil = DB::select('SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, 
+    kl.nama AS nama_kelas, kl.gol, IF(kl.gol = "DB",(jh.debit - jh.kredit),-(jh.debit - jh.kredit)) AS saldo_akhir 
     FROM jurnal_sejarahs jh 
     INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
     INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
