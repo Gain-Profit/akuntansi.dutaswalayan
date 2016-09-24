c3172b214cb70e5f25781da80334f3feedc9983d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-24 19:45:10 +0700

    laba komponent vuejs

---
+++ resources/assets/js/components/Laba.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="text-center">Laba Rugi</h2>
+                <div class="row">
+                    <div class="col-xs-6">
+                        Unit : 
+                        <div class="btn-group" role="group" aria-label="...">
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ perusahaan.nama }}
+                                <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li v-for="unit in perusahaans"><a @click="ubahPerusahaan(unit.kode)">{{ unit.nama }}</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-6 text-right">
+                        Periode : 
+                        <div class="btn-group" role="group" aria-label="...">
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ bulan }}
+                                <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li v-for="n in 12"><a @click="ubahBulan(n+1)">{{ n+1 }}</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ tahun }}
+                                <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li v-for="n in 5"><a @click="ubahTahun(n + tahunSekarang - 4)">{{ n + tahunSekarang - 4 }}</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>                                
+            </div>
+
+            <div class="panel-body">
+                <div v-if="labas.length === 0">
+                    <div class="alert alert-warning"><strong>Maaf</strong> Laba Rugi pada Periode ini tidak di temukan</div>
+                </div>
+            
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nama Akun</th>
+                            <th class="text-right">Saldo</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <td><strong>Laba (Rugi)</strong></td>
+                            <td class="text-right">
+                                <strong>{{ totalLaba  | currencyDisplay }}</strong>
+                            </td>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr v-for="laba in labas">
+                            <td>{{ laba.nama_kiraan }}</td>
+                            <td class="text-right">
+                                <strong>{{ laba.saldo_akhir | currencyDisplay }}</strong>
+                            </td>                            
+                        </tr>                            
+                    </tbody>
+                </table>                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                labas : [],
+                totalLaba : 0,
+                perusahaan : { kode: 'BR001', nama: 'DUTA SWALAYAN'},
+                perusahaans : [
+                    { kode: 'BR001', nama: 'DUTA SWALAYAN'},
+                    { kode: 'PS001', nama: 'DUTA GROSIR'}
+                ],
+                tahun : new Date().getFullYear(),
+                bulan : new Date().getMonth() + 1,
+                tahunSekarang : new Date().getFullYear(),                
+            }
+        },
+        ready() {
+            this.getLabas();            
+        },
+        methods: {
+            getLabas() {
+                this.$http.get('/api/laba/' + this.perusahaan.kode + '/' + this.tahun + this.bulan )
+                        .then(response => {                            
+                            this.labas = response.data;
+                            this.hanyaBernilai();                            
+                        });
+            },
+            hanyaBernilai() {
+                this.labas  = _.filter(this.labas, function(v){
+                    return v.saldo_akhir != 0;
+                });
+                this.totalLaba = _.sumBy(this.labas, 'saldo_akhir');
+            },
+            ubahBulan($bulan) {
+                if(this.bulan != $bulan) {
+                    this.bulan = $bulan;
+                    this.getLabas();
+                }
+            },
+            ubahTahun($tahun) {
+                if(this.tahun != $tahun) {
+                    this.tahun = $tahun;
+                    this.getLabas();
+                }
+            },
+            ubahPerusahaan($kode) {
+                if(this.perusahaan.kode != $kode) {
+                    this.perusahaan = _.find(this.perusahaans, { 'kode' : $kode });
+                    this.getLabas();
+                }
+            }            
+        },
+        filters: {
+            currencyDisplay: {
+                read: function(val) {
+                    var hasil = Math.abs(val).toLocaleString();
+                    if (val < 0) {
+                        hasil = '(' + hasil + ')';
+                    }
+                    return hasil;
+                },                
+            }
+        }        
+    }
+</script>
