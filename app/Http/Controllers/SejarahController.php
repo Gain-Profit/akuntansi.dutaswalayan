37a57ef0171f739d6495f0314397c7345985fb05
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-09-24 15:54:55 +0700

    reformat query.

--- app/Http/Controllers/SejarahController.php
+++ app/Http/Controllers/SejarahController.php
@@ -21,15 +21,15 @@ class SejarahController extends Controller
         $bulan = substr($period, 4);
 
         $hasil = DB::select(
-        'SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, kl.nama AS nama_kelas, kl.gol, 
-        IF(kl.gol = "DB",(jh.saldo_awal + jh.debit - jh.kredit),-(jh.saldo_awal + jh.debit - jh.kredit)) AS saldo_akhir 
-        FROM jurnal_sejarahs jh 
-        INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
-        INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
-        INNER JOIN kelas kl ON sk.kelas_id = kl.id 
-        WHERE jh.perusahaan_id = ? AND jh.tahun = ? AND jh.bulan = ? AND kl.jenis = "NR"',
-        [$comp, $tahun, $bulan]
-    );
+            'SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, kl.nama AS nama_kelas, kl.gol, 
+            IF(kl.gol = "DB",(jh.saldo_awal + jh.debit - jh.kredit),-(jh.saldo_awal + jh.debit - jh.kredit)) AS saldo_akhir 
+            FROM jurnal_sejarahs jh 
+            INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
+            INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
+            INNER JOIN kelas kl ON sk.kelas_id = kl.id 
+            WHERE jh.perusahaan_id = ? AND jh.tahun = ? AND jh.bulan = ? AND kl.jenis = "NR"',
+            [$comp, $tahun, $bulan]
+        );
     
         return $hasil;
     }
@@ -39,14 +39,16 @@ class SejarahController extends Controller
         $tahun = substr($period, 0, 4);
         $bulan = substr($period, 4);
 
-        $hasil = DB::select('SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, 
-        kl.nama AS nama_kelas, kl.gol, IF(kl.gol = "DB",(jh.debit - jh.kredit),-(jh.debit - jh.kredit)) AS saldo_akhir 
-        FROM jurnal_sejarahs jh 
-        INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
-        INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
-        INNER JOIN kelas kl ON sk.kelas_id = kl.id 
-        WHERE jh.perusahaan_id = ? AND jh.tahun = ? AND jh.bulan = ? AND kl.jenis = "LR"',
-        [$comp, $tahun, $bulan]);
+        $hasil = DB::select(
+            'SELECT jh.perusahaan_id, jh.kiraan_id, kr.nama AS nama_kiraan, sk.nama AS nama_kelas_sub, 
+            kl.nama AS nama_kelas, kl.gol, IF(kl.gol = "DB",(jh.debit - jh.kredit),-(jh.debit - jh.kredit)) AS saldo_akhir 
+            FROM jurnal_sejarahs jh 
+            INNER JOIN kiraans kr ON (jh.kiraan_id = kr.id AND jh.perusahaan_id = kr.perusahaan_id) 
+            INNER JOIN kelas_subs sk ON kr.kelas_sub_id = sk.id 
+            INNER JOIN kelas kl ON sk.kelas_id = kl.id 
+            WHERE jh.perusahaan_id = ? AND jh.tahun = ? AND jh.bulan = ? AND kl.jenis = "LR"',
+            [$comp, $tahun, $bulan]
+        );
     
         return $hasil;
     }
