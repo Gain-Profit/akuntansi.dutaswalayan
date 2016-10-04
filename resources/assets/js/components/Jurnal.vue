c5b17fa901158a7eeb2fdcd4a5d717e854542407
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-10-04 21:30:13 +0700

    compress jurnal component

--- resources/assets/js/components/Jurnal.vue
+++ resources/assets/js/components/Jurnal.vue
@@ -1,139 +1,134 @@
 <template>
-    <div class="container">
-        <div class="m-b-none" v-if="perusahaans.length === 0">
-            <div class="alert alert-warning text-center"><strong>Maaf Anda tidak terhubung dengan unit manapun</strong></div>
-        </div>
-        <div class="panel panel-default m-b-none" v-if="perusahaans.length > 0">
-            <div class="panel-heading">
-                <h2 class="text-center">Daftar Jurnal</h2>
-                <div class="row">
-                    <div class="col-xs-6">
-                        Unit :
-                        <div class="btn-group" role="group" aria-label="...">
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ perusahaan.nama }} <span class="caret"></span></button>
-                                <ul class="dropdown-menu">
-                                    <li v-for="unit in perusahaans"><a @click="ubahPerusahaan(unit.id)">{{ unit.nama }}</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-6 text-right">
-                        Periode :
-                        <div class="btn-group" role="group" aria-label="...">
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ bulan }} <span class="caret"></span></button>
-                                <ul class="dropdown-menu">
-                                    <li v-for="n in 12"><a @click="ubahBulan(n+1)">{{ n+1 }}</a></li>
-                                </ul>
-                            </div>
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ tahun }} <span class="caret"></span></button>
-                                <ul class="dropdown-menu">
-                                    <li v-for="n in 5"><a @click="ubahTahun(n + tahunSekarang - 4)">{{ n + tahunSekarang - 4 }}</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="panel-body">
-                <div class="m-b-none" v-if="jurnals.length === 0">
-                    <div class="alert alert-warning"><strong>Maaf</strong> jurnal pada Periode ini tidak di temukan</div>
-                </div>
-                <div class="m-b-none" v-if="jurnals.length > 0">
-                    <nav aria-label="...">
-                        <ul class="pager">
-                            <li class="{{ prev }}"><a @click="ubahHalaman(halaman - 1)"><span aria-hidden="true">&larr;</span> Newer</a></li>
-                            <li class="next"><a @click="ubahHalaman(halaman + 1)">Older <span aria-hidden="true">&rarr;</span></a></li>
-                        </ul>
-                    </nav>
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Aksi</th>
-                                    <th>Tanggal</th>
-                                    <th>Keterangan</th>
-                                    <th>Referensi</th>
-                                    <th class="text-right">Nilai</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="jurnal in jurnals">
-                                    <td>
-                                        <button type="button" class="btn btn-primary btn-xs" @click="show(jurnal)">Detail</button>
-                                    </td>
-                                    <td>{{ jurnal.tanggal }}</td>
-                                    <td>{{ jurnal.keterangan | cutString '70' }}</td>
-                                    <td>{{ jurnal.referensi }}</td>
-                                    <td class="text-right">
-                                        <strong>{{ jurnal.nilai | currencyDisplay }}</strong>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <!-- Create Jurnal Modal -->
-                <div class="modal fade" id="modal-show-jurnal" tabindex="-1" role="dialog">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-
-                                <h4 class="modal-title">
-                                    Detail Jurnal
-                                </h4>
-                            </div>
-
-                            <div class="modal-body">
-                                {{ showForm.keterangan }}
-                                <table class="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Kode</th>
-                                            <th>Akun</th>
-                                            <th class="text-right">Debit</th>
-                                            <th class="text-right">Kredit</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <td colspan=2><strong>Total</strong></td>
-                                            <td class="text-right">
-                                                <strong>{{ showForm.totalDebit  | currencyDisplay }}</strong>
-                                            </td>
-                                            <td class="text-right">
-                                                <strong>{{ showForm.totalKredit | currencyDisplay }}</strong>
-                                            </td>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr v-for="detail in showForm.details">
-                                            <td>{{ detail.kiraan_id }}</td>
-                                            <td>{{ detail.nama}}</td>
-                                            <td class="text-right">
-                                                <strong>{{ detail.debit | currencyDisplay }}</strong>
-                                            </td>
-                                            <td class="text-right">
-                                                <strong>{{ detail.kredit | currencyDisplay }}</strong>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-
-                            <!-- Modal Actions -->
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+<div class="m-b-none" v-if="perusahaans.length === 0">
+<div class="alert alert-warning text-center"><strong>Maaf Anda tidak terhubung dengan unit manapun</strong></div>
+</div>
+<div class="panel panel-default m-b-none" v-if="perusahaans.length > 0">
+<div class="panel-heading">
+<h2 class="text-center">Daftar Jurnal</h2>
+<div class="row">
+<div class="col-xs-6">
+Unit :
+<div class="btn-group" role="group" aria-label="...">
+<div class="btn-group" role="group">
+<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ perusahaan.nama }} <span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li v-for="unit in perusahaans"><a @click="ubahPerusahaan(unit.id)">{{ unit.nama }}</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-xs-6 text-right">
+Periode :
+<div class="btn-group" role="group" aria-label="...">
+<div class="btn-group" role="group">
+<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ bulan }} <span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li v-for="n in 12"><a @click="ubahBulan(n+1)">{{ n+1 }}</a></li>
+</ul>
+</div>
+<div class="btn-group" role="group">
+<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ tahun }} <span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li v-for="n in 5"><a @click="ubahTahun(n + tahunSekarang - 4)">{{ n + tahunSekarang - 4 }}</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="panel-body">
+<div class="m-b-none" v-if="jurnals.length === 0">
+<div class="alert alert-warning"><strong>Maaf</strong> jurnal pada Periode ini tidak di temukan</div>
+</div>
+<div class="m-b-none" v-if="jurnals.length > 0">
+<nav aria-label="...">
+<ul class="pager">
+<li class="{{ prev }}"><a @click="ubahHalaman(halaman - 1)"><span aria-hidden="true">&larr;</span> Newer</a></li>
+<li class="next"><a @click="ubahHalaman(halaman + 1)">Older <span aria-hidden="true">&rarr;</span></a></li>
+</ul>
+</nav>
+<div class="table-responsive">
+<table class="table table-striped">
+<thead>
+<tr>
+<th>Aksi</th>
+<th>Tanggal</th>
+<th>Keterangan</th>
+<th>Referensi</th>
+<th class="text-right">Nilai</th>
+</tr>
+</thead>
+<tbody>
+<tr v-for="jurnal in jurnals">
+<td>
+<button type="button" class="btn btn-primary btn-xs" @click="show(jurnal)">Detail</button>
+</td>
+<td>{{ jurnal.tanggal }}</td>
+<td>{{ jurnal.keterangan | cutString '70' }}</td>
+<td>{{ jurnal.referensi }}</td>
+<td class="text-right">
+<strong>{{ jurnal.nilai | currencyDisplay }}</strong>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+<div class="modal fade" id="modal-show-jurnal" tabindex="-1" role="dialog">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+<h4 class="modal-title">
+Detail Jurnal
+</h4>
+</div>
+<div class="modal-body">
+{{ showForm.keterangan }}
+<table class="table table-striped">
+<thead>
+<tr>
+<th>Kode</th>
+<th>Akun</th>
+<th class="text-right">Debit</th>
+<th class="text-right">Kredit</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td colspan=2><strong>Total</strong></td>
+<td class="text-right">
+<strong>{{ showForm.totalDebit  | currencyDisplay }}</strong>
+</td>
+<td class="text-right">
+<strong>{{ showForm.totalKredit | currencyDisplay }}</strong>
+</td>
+</tr>
+</tfoot>
+<tbody>
+<tr v-for="detail in showForm.details">
+<td>{{ detail.kiraan_id }}</td>
+<td>{{ detail.nama}}</td>
+<td class="text-right">
+<strong>{{ detail.debit | currencyDisplay }}</strong>
+</td>
+<td class="text-right">
+<strong>{{ detail.kredit | currencyDisplay }}</strong>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 </template>
 
 <script>
@@ -192,21 +187,21 @@
             },
             ubahBulan(bulan) {
                 if(this.bulan != bulan) {
-                    this.halaman = 1;
+                    this.ubahHalaman(1);
                     this.bulan = bulan;
                     this.getJurnals();
                 }
             },
             ubahTahun(tahun) {
                 if(this.tahun != tahun) {
-                    this.halaman = 1;
+                    this.ubahHalaman(1);
                     this.tahun = tahun;
                     this.getJurnals();
                 }
             },
             ubahPerusahaan(kode) {
                 if(this.perusahaan.kode != kode) {
-                    this.halaman = 1;
+                    this.ubahHalaman(1);
                     this.perusahaan = _.find(this.perusahaans, { 'id' : kode });
                     this.getJurnals();
                 }
