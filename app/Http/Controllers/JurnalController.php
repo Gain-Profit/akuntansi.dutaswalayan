bea8cf3fe1efbfd876d536fea8a5e7f32c6cb245
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-10-04 21:03:36 +0700

    jurnal controller

---
+++ app/Http/Controllers/JurnalController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\JurnalDetail;
+use App\Models\Jurnal;
+use App\Http\Requests;
+use Gate;
+use DB;
+
+class JurnalController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('jurnal.index');
+    }
+
+    public function getJurnals($comp, $period, $hal, $random)
+    {
+        if (Gate::denies('show-unit', $comp)) {
+            abort(404);
+        }
+
+        $tahun = substr($period, 0, 4);
+        $bulan = substr($period, 4);
+
+        $limit = 20;
+        $offset = ($hal - 1) * $limit;
+        
+        $jurnals = Jurnal::where('perusahaan_id',$comp)
+            ->whereMonth('tanggal',$bulan)
+            ->whereYear('tanggal',$tahun)
+            ->orderBy('tanggal','desc')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
+
+        return $jurnals;
+    }
+
+    public function getJurnalDetail($comp, $index, $random)
+    {
+        if (Gate::denies('show-unit', $comp)) {
+            abort(404);
+        }
+
+        // $jurnalDetail = JurnalDetail::where('perusahaan_id',$comp)
+        //     ->where('jurnal_id', $index)
+        //     ->orderBy('urut')
+        //     ->get();
+
+        // return $jurnalDetail;
+
+        $hasil = DB::select(
+            'SELECT jd.kiraan_id, kr.nama, jd.debit, jd.kredit FROM jurnal_details jd
+            INNER JOIN kiraans kr ON jd.kiraan_id = kr.id AND kr.perusahaan_id = jd.perusahaan_id 
+            WHERE jd.perusahaan_id = ? AND jd.jurnal_id = ? ORDER BY jd.urut',
+            [$comp, $index]
+        );
+    
+        return $hasil;
+
+    }
+
+}
