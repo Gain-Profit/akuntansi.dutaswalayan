76afe2f4d333e9234fc0811e7df0f982cff3625a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-10-04 21:04:30 +0700

    menu jurnal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,7 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jurnal <span
                                     class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Jurnal Umum</a></li>
-                            <li><a href="#">Transaksi Jurnal</a></li>
+                            <li><a href="{{ url('/jurnal') }}">Daftar Transaksi Jurnal</a></li>
                         </ul>
                     </li>
                     <li class="dropdown">
