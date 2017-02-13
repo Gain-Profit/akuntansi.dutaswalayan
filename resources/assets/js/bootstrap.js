67622046c57680fb375e450249b7cbaf4231d312
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-02-13 20:17:26 +0700

    gunakan axios untuk request

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -25,7 +25,7 @@ window.Vue = require('vue');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-Vue.http.interceptors.push((request, next) => {
+window.axios = require('axios');
 
 window.axios.defaults.headers.common = {
     'X-CSRF-TOKEN': window.Laravel.csrfToken,
