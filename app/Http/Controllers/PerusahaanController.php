f909cf7bddb0702957f7dfe4d13394e68e2dc2b6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-02-15 15:35:31 +0700

    cache list perusahaan user 7 hari.

--- app/Http/Controllers/PerusahaanController.php
+++ app/Http/Controllers/PerusahaanController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
 use App\Http\Requests;
+use Carbon\Carbon;
+use Cache;
+use DB;
 
 class PerusahaanController extends Controller
 {
@@ -15,9 +17,17 @@ class PerusahaanController extends Controller
 
     public function getPerusahaans($random)
     {
-        return DB::select('SELECT p.id, p.nama 
-            FROM perusahaans p 
-            INNER JOIN perusahaan_user pu ON p.id = pu.perusahaan_id
-            WHERE pu.user_id = ?', [auth()->user()->id]);
+        
+        $user_id = auth()->user()->id;
+        
+        $minutes = Carbon::now()->addDays(7);
+
+        $hasil = Cache::remember('perusahaans_' . $user_id, $minutes, function() use ($user_id){
+            return DB::select('SELECT p.id, p.nama 
+                FROM perusahaans p 
+                INNER JOIN perusahaan_user pu ON p.id = pu.perusahaan_id
+                WHERE pu.user_id = ?', [$user_id]);  
+        });         
+        return $hasil;
     }
 }
